ec3382d97cf3ef70ff1241866398969779804817
Kenneth Cartaña Mendoza <kenneth.cartana@ypsomed.com>
2024-12-03 10:36:51 +0100

    Added custom material theme

--- src/composeApp/src/commonMain/kotlin/com/knthcame/myhealthkmp/AppComposable.kt
+++ src/composeApp/src/commonMain/kotlin/com/knthcame/myhealthkmp/AppComposable.kt
@@ -4,9 +4,8 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.knthcame.myhealthkmp.ui.theme.MyHealthTheme
 import myhealthkmp.composeapp.generated.resources.Res
 import myhealthkmp.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
@@ -23,7 +23,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MyHealthTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(
