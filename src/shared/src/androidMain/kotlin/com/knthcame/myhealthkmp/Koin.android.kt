2eee2719058879f1e8c995effaa0355f88b10cf0
Kenneth Cartanyà Mendoza <39163040+Knthcame@users.noreply.github.com>
2026-02-11 09:20:24 +0100

    Bump to Android 16

--- src/shared/src/androidMain/kotlin/com/knthcame/myhealthkmp/Koin.android.kt
+++ src/shared/src/androidMain/kotlin/com/knthcame/myhealthkmp/Koin.android.kt
@@ -3,7 +3,7 @@ package com.knthcame.myhealthkmp
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-/** The koin module with the dependencies to inject from composeApp.androidMain. */
+/** The koin module with the dependencies to inject from shared.androidMain. */
 actual val platformModule: Module = module {
     // Nothing here yet
 }
